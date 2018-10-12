2b5ba816f70850ae912c93e02afb3ca1069c17de
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-10-12 12:11:57 -0400

    Updated from Brython Server: 10/12/2018 12:11:57 PM

--- cryptography.py
+++ cryptography.py
@@ -10,8 +10,8 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-for x in associations:
-    print(associations.find(x))
+#for x in associations:
+    #print(associations.find(x))
     
 #inputs
 toDo = input("Enter e to encrypt, d to decrypt, or q to quit: ")
@@ -21,12 +21,17 @@ key = input("Key: ")
 #Possibilities
 if toDo == "e":
     #encrypt
-    
+    for unit in message:
+        unitList = list(unit)
+        for i in unitList:
+            part = associations.find(i)
+        print(part)
     
     
     
 elif toDo == "d":
     #decrypt
+    print("d")
     
 elif toDo == "q":
     print("Goodbye!")
